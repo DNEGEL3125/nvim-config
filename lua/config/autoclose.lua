c96d92520a07588a7253ca81cf8fa8205b6b60b9
DNEGEL3125 <2839212140@qq.com>
2024-11-23 09:35:23 +0800

    feat(lspconfig): Add C and C++ support

--- lua/config/autoclose.lua
+++ lua/config/autoclose.lua
@@ -1,6 +1,7 @@
 require("autoclose").setup({
    keys = {
       ["{"] = { escape = true, close = true, pair = "{}", disabled_filetypes = {} },
+        ["'"] = { escape = false, close = false },
    },
     options = {
         disabled_filetypes = {"text", "markdown"},
--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,5 +1,16 @@
+-- Ensure nvim-lspconfig is required
+local lspconfig = require('lspconfig')
+
+-- Configure clangd
+lspconfig.clangd.setup({
+  cmd = { "clangd" }, -- Adjust the path if clangd isn't in your PATH
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(), -- if using nvim-cmp
+})
+
 -- Rust
-require('lspconfig').rust_analyzer.setup({
+lspconfig.rust_analyzer.setup({
   on_attach = function(client, bufnr)
     -- You can add additional configuration here if needed
   end,
