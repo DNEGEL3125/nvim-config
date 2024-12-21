e33acf8e9a0180b776fe405dd3f205aec70493db
DNEGEL3125 <2839212140@qq.com>
2024-12-22 01:56:09 +0800

    feat(lsp): java support

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,6 +1,11 @@
 -- Ensure nvim-lspconfig is required
 local lspconfig = require('lspconfig')
 
+-- Enable the following if you want to use the capabilities of language server
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local java_home = os.getenv("JAVA_HOME")
+
 -- Configure clangd
 lspconfig.clangd.setup({
   cmd = { "clangd" }, -- Adjust the path if clangd isn't in your PATH
@@ -16,4 +21,38 @@ lspconfig.rust_analyzer.setup({
   end,
   -- Optionally, specify the path to `rust-analyzer` if it's not in PATH:
   -- cmd = { "/path/to/rust-analyzer" },
+})
+
+-- Java
+
+lspconfig.jdtls.setup({
+--   cmd = {
+--     "java",
+--     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+--     "-Dosgi.bundles.defaultStartLevel=4",
+--     "-Declipse.product=org.eclipse.jdt.ls.core.product",
+--     "-Dlog.protocol=true",
+--     "-Dlog.level=ALL",
+--     "-Xms1g",
+--     "--add-modules=ALL-SYSTEM",
+--     "--add-opens", "java.base/java.util=ALL-UNNAMED",
+--     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+--     "-jar", launcher_jar,
+--     "-configuration", jdtls_home .. "/config_linux",
+--     "-data", vim.fn.getcwd() .. "/.jdtls-workspace",
+--   },
+  root_dir = require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git"),
+  settings = {
+    java = {
+      home = java_home, -- Set this to your JDK home directory
+      -- configuration for signature help
+      signatureHelp = { enabled = true },
+      -- configuration for organize imports on save
+      saveActions = { organizeImports = true },
+      -- configuration for compiler
+      compiler = {
+        complianceLevel = "17", -- Set your Java version
+      },
+    },
+  },
 })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,28 @@
-return { "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim"
-  },
-  config = function()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
+return { 
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "williamboman/nvim-lsp-installer",
+    },
+    config = function()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    require('mason').setup()
-    local mason_lspconfig = require 'mason-lspconfig'
-    mason_lspconfig.setup {
-        ensure_installed = { "pyright" }
-    }
-    require("lspconfig").pyright.setup {
-        capabilities = capabilities,
-    }
-  end
+        require('mason').setup()
+        local mason_lspconfig = require 'mason-lspconfig'
+        mason_lspconfig.setup {
+            ensure_installed = { "pyright" }
+        }
+        require("lspconfig").pyright.setup {
+            capabilities = capabilities,
+        }
+
+        -- Install missing servers on startup.
+        require("nvim-lsp-installer").setup {
+            ui = {
+                border = "rounded",
+            },
+        }
+    end
 }
