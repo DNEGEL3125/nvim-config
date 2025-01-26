e7cc161ddbf1ec58370b2d7f6cc7694672fe8933
DNEGEL3125 <2839212140@qq.com>
2025-01-26 20:50:10 +0800

    style(lua/init.lua): formatting

--- lua/init.lua
+++ lua/init.lua
@@ -1,25 +1,25 @@
 require("config.lazy")
-require "config.treesitter-config" -- Added this line to initial file
-require "config.nvim-tree-config"
-require "config.md-preview"
-require "config.statusline"
-require "config.colorscheme"
-require "config.lspconfig"
-require "config.telescope"
-require "config.debug"
-require "config.formatter"
+require("config.treesitter-config") -- Added this line to initial file
+require("config.nvim-tree-config")
+require("config.md-preview")
+require("config.statusline")
+require("config.colorscheme")
+require("config.lspconfig")
+require("config.telescope")
+require("config.debug")
+require("config.formatter")
 
 -- Use `:Diag` to show warnings/errors
-vim.api.nvim_create_user_command('Diag', function()
-    vim.diagnostic.open_float(nil, { focusable = false, border = "rounded" })
+vim.api.nvim_create_user_command("Diag", function()
+	vim.diagnostic.open_float(nil, { focusable = false, border = "rounded" })
 end, {})
 
 -- Use `K` to show type info
 if vim.g.vscode then
-    -- Do nothing
+	-- Do nothing
 else
-    vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 end
 
 -- Go to definition
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
