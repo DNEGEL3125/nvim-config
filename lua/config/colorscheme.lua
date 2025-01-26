69b610a74c758be852220869d47e2626ec253506
DNEGEL3125 <2839212140@qq.com>
2025-01-26 20:52:14 +0800

    style(lua/config/colorscheme.lua): formatting

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,19 +1,19 @@
 require("cyberdream").setup({
-    -- Enable transparent background
-    transparent = true,
+	-- Enable transparent background
+	transparent = true,
 
-    -- Enable italics comments
-    italic_comments = false,
+	-- Enable italics comments
+	italic_comments = false,
 
-    -- Replace all fillchars with ' ' for the ultimate clean look
-    hide_fillchars = false,
+	-- Replace all fillchars with ' ' for the ultimate clean look
+	hide_fillchars = false,
 
-    -- Modern borderless telescope theme - also applies to fzf-lua
-    borderless_telescope = true,
+	-- Modern borderless telescope theme - also applies to fzf-lua
+	borderless_telescope = true,
 
-    -- Set terminal colors used in `:terminal`
-    terminal_colors = true,
+	-- Set terminal colors used in `:terminal`
+	terminal_colors = true,
 
-    -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
-    cache = false,
+	-- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+	cache = false,
 })
