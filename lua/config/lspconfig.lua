1fe7e71475c87b68d9cfa21f5c2302e25ff58f33
DNEGEL3125 <2839212140@qq.com>
2025-02-04 17:04:16 +0800

    feat(lsp): rust Auto-Import

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,6 +1,8 @@
 -- Ensure nvim-lspconfig is required
 local lspconfig = require("lspconfig")
 
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
 -- Rename the variable under your cursor.
 --  Most Language Servers support renaming across files, etc.
 vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
@@ -10,7 +12,7 @@ lspconfig.clangd.setup({
 	cmd = { "clangd" }, -- Adjust the path if clangd isn't in your PATH
 	filetypes = { "c", "cpp", "objc", "objcpp" },
 	root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-	capabilities = require("cmp_nvim_lsp").default_capabilities(), -- if using nvim-cmp
+	capabilities = cmp_nvim_lsp.default_capabilities(), -- if using nvim-cmp
 })
 
 -- Rust
@@ -18,6 +20,5 @@ lspconfig.rust_analyzer.setup({
 	on_attach = function(client, bufnr)
 		-- You can add additional configuration here if needed
 	end,
-	-- Optionally, specify the path to `rust-analyzer` if it's not in PATH:
-	-- cmd = { "/path/to/rust-analyzer" },
+    capabilities = cmp_nvim_lsp.default_capabilities(),
 })
