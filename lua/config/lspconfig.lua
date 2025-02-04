fa858ff8b2eea1f33b131897f4550a01f4581ab0
DNEGEL3125 <2839212140@qq.com>
2025-02-04 17:24:36 +0800

    refactor(lsp): organize the configuration.
    - Move pyright config to `lua/config/lspconfig.lua`
    - Create a shared `capabilities`

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,24 +1,34 @@
 -- Ensure nvim-lspconfig is required
 local lspconfig = require("lspconfig")
-
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local mason_lspconfig = require("mason-lspconfig")
+require("mason").setup()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Rename the variable under your cursor.
---  Most Language Servers support renaming across files, etc.
+-- Most Language Servers support renaming across files, etc.
 vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+
+-- Configure pyright
+mason_lspconfig.setup({
+	ensure_installed = { "pyright" },
+})
+lspconfig.pyright.setup({
+	capabilities = capabilities,
+})
 
 -- Configure clangd
 lspconfig.clangd.setup({
 	cmd = { "clangd" }, -- Adjust the path if clangd isn't in your PATH
 	filetypes = { "c", "cpp", "objc", "objcpp" },
 	root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-	capabilities = cmp_nvim_lsp.default_capabilities(), -- if using nvim-cmp
+	capabilities = capabilities, -- if using nvim-cmp
 })
 
--- Rust
+-- Configure rust-analyzer
 lspconfig.rust_analyzer.setup({
 	on_attach = function(client, bufnr)
 		-- You can add additional configuration here if needed
 	end,
-    capabilities = cmp_nvim_lsp.default_capabilities(),
+	capabilities = capabilities,
 })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,17 +4,4 @@ return {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
-	config = function()
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-		require("mason").setup()
-		local mason_lspconfig = require("mason-lspconfig")
-		mason_lspconfig.setup({
-			ensure_installed = { "pyright" },
-		})
-		require("lspconfig").pyright.setup({
-			capabilities = capabilities,
-		})
-	end,
 }
