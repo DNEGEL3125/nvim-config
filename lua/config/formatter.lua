70d01e66e4f3cf6e18d044eb3c565953f2ce5627
DNEGEL3125 <2839212140@qq.com>
2025-02-09 13:31:00 +0800

    feat(formatter): enable formatting on save
    Enable formatting on save for:
    - javascript
    - typescript
    - lua
    - rust

--- lua/config/formatter.lua
+++ lua/config/formatter.lua
@@ -8,4 +8,23 @@ require("conform").setup({
 		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 	},
+	format_on_save = function(bufnr)
+		-- Define the list of languages for which you want to enable format-on-save
+		local enabled_languages = {
+			"javascript",
+			"typescript",
+			"lua",
+			"rust",
+		}
+
+		-- Get the file type of the current file
+		local filetype = vim.bo[bufnr].filetype
+
+		-- Check if the current file type is in the `enabled_languages`
+		if vim.tbl_contains(enabled_languages, filetype) then
+			return { timeout_ms = 500, lsp_fallback = true } -- Enable formatting
+		else
+			return false -- Disable formatting
+		end
+	end,
 })
