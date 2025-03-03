9790c22f0d56eaf2387574303ab4ea7678a7b7ea
DNEGEL3125 <2839212140@qq.com>
2025-03-04 00:57:40 +0800

    feat(highlight): ensure install rust and go

--- lua/config/highlight.lua
+++ lua/config/highlight.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup({
 	-- Add a language of your choice
-	ensure_installed = { "cpp", "python", "lua", "java", "javascript", "markdown", "markdown_inline" },
+	ensure_installed = { "cpp", "python", "lua", "java", "javascript", "go", "rust", "markdown", "markdown_inline" },
 	sync_install = false,
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
