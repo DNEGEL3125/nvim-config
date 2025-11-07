54c23cd93b04562e2aa6f55553a5db2a3e4c0090
DNEGEL3125 <2839212140@qq.com>
2025-11-07 20:46:19 +0800

    chore: remove needless cmake-tools config

--- lua/init.lua
+++ lua/init.lua
@@ -6,7 +6,6 @@ require("config.colorscheme")
 require("config.lsp")
 require("config.debug")
 require("config.formatter")
-require("config.cmake-tools")
 
 -- Use `:Diag` to show warnings/errors
 vim.api.nvim_create_user_command("Diag", function()
