8b1ef8bc1a4978eed69f3ec4e60728022f5c8e8e
DNEGEL3125 <2839212140@qq.com>
2025-11-07 22:06:55 +0800

    refactor: move cyberdream options to `plugins/colorscheme.lua`

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,5 +10,24 @@ return {
 		"scottmckendry/cyberdream.nvim",
 		lazy = false,
 		priority = 1000,
+		opts = {
+			-- Enable transparent background
+			transparent = true,
+
+			-- Enable italics comments
+			italic_comments = true,
+
+			-- Replace all fillchars with ' ' for the ultimate clean look
+			hide_fillchars = false,
+
+			-- Modern borderless theme - also applies to fzf-lua
+			borderless_pickers = true,
+
+			-- Set terminal colors used in `:terminal`
+			terminal_colors = true,
+
+			-- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+			cache = false,
+		},
 	},
 }
