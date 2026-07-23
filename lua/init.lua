aa04dfe7d7ebb87217cad3ebb54623282a71a4d0
DNEGEL3125 <2839212140@qq.com>
2026-07-24 01:33:59 +0800

    fix(highlight): fix treesitter error

--- lua/init.lua
+++ lua/init.lua
@@ -4,7 +4,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("config.lazy")
-require("config.highlight")
 require("config.nvim-tree")
 require("config.statusline")
 require("config.lsp")
--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,7 +1,31 @@
 local M = {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
+	branch = "main", -- 關鍵：從 master 改為 main
+	build = ":TSUpdate",
+	--	build = function()
+	--		require("nvim-treesitter.install").update({ with_sync = true })()
+	--	end,
+	config = function()
+		require("nvim-treesitter").setup({
+			-- Add a language of your choice
+			ensure_installed = { "cpp", "python", "lua", "java", "javascript", "go", "rust" },
+			sync_install = false,
+			ignore_install = { "" }, -- List of parsers to ignore installing
+			highlight = {
+				enable = true, -- false will disable the whole extension
+				disable = { "" }, -- list of language that will be disabled
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true, disable = { "yaml" } },
+			rainbow = {
+				enable = true,
+				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+				max_file_lines = nil, -- Do not enable for files with more than n lines, int
+				-- colors = {}, -- table of hex strings
+				-- termcolors = {} -- table of colour name strings
+			},
+		})
 	end,
 }
 
